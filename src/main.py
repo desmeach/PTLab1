bb72e83297b1bb82110b1faef0794ceaffaef26f
Vladislav Naryzhny <desmeach@gmail.com>
2023-11-08 10:22:37 +0300

    Added feature to parse few path in -p param to read data from different files and ext

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import argparse
 import sys
+import os
 from QuartileRating import QuartileRating
+from CalcRating import CalcRating
 from JsonDataReader import JsonDataReader
+from TextDataReader import TextDataReader
 
 
 def get_path_from_arguments(args) -> str:
@@ -14,12 +17,23 @@ def get_path_from_arguments(args) -> str:
 
 
 def main():
-    path = get_path_from_arguments(sys.argv[1:])
-    json_reader = JsonDataReader()
-    students = json_reader.read(path)
-    print("Students: ", students)
-    third_quartile_students = QuartileRating(students).calc()
-    print("Third quartile students: ", third_quartile_students)
+    paths = get_path_from_arguments(sys.argv[1:])
+    paths = paths.split(' ')
+    for path in paths:
+        filename, file_extension = os.path.splitext(path)
+        if file_extension == '.json':
+            json_reader = JsonDataReader()
+            students = json_reader.read(path)
+            print("Students: ", students)
+            third_quartile_students = QuartileRating(students).calc()
+            print("Third quartile students: ", third_quartile_students)
+        elif file_extension == '.txt':
+            reader = TextDataReader()
+            students = reader.read(path)
+            print("Students: ", students)
+            rating = CalcRating(students).calc()
+            print("Rating: ", rating)
+        print('###################')
 
 
 if __name__ == "__main__":
